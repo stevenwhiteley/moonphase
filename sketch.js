dabb3ca46c20c8f4a2c604116479a98c88f77256
Steven <stevenwhiteley4@gmail.com>
2020-02-18 15:40:20 -0800

    vid

--- sketch.js
+++ sketch.js
@@ -6,12 +6,12 @@ let theta = 0;
 
 
 function setup() {
-  createCanvas(710, 400, WEBGL);
+  createCanvas(1000, 900, WEBGL);
   noStroke();
   fill(200);
   moon = loadModel('assets/moon.obj', true);
   img = loadImage('assets/download.jpg');
-  vid = createVideo(['assets/torso.mov']);
+  vid = createVideo(['assets/lumenglitches.mp4']);
   vid.loop();
   vid.hide();
 
@@ -19,16 +19,18 @@ function setup() {
 
 function draw() {
   //noStroke();
-  background(0);
+  background(255);
   // const dirY = (mouseY / height - 0.5) * 8;
   // const dirX = (mouseX / width - 0.5) * 8;
   // directionalLight(204, 204, 204, dirX*2, dirY*2, .1);
-  translate(0 * radius, 0, 0);
+  translate(0 * radius, 0, 5);
   rotateZ(theta * 1);
   rotateX(theta * 1.01);
   rotateY(theta * 1.01);
   texture(vid);
-  model(moon, mouseX, mouseY);
+  scale(3);
+  model(moon);
+
     theta += 0.05;
 
 }
