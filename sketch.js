dffa05a4cc857ee5a5072d27386c426a84455ce1
Steven <stevenwhiteley4@gmail.com>
2020-02-18 15:31:45 -0800

    addedvid

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 const radius = 200;
 let moon;
 let img;
+let vid
+let theta = 0;
+
 
 function setup() {
   createCanvas(710, 400, WEBGL);
@@ -8,17 +11,24 @@ function setup() {
   fill(200);
   moon = loadModel('assets/moon.obj', true);
   img = loadImage('assets/download.jpg');
+  vid = createVideo(['assets/torso.mov']);
+  vid.loop();
+  vid.hide();
+
 }
 
 function draw() {
-  noStroke();
+  //noStroke();
   background(0);
-  const dirY = (mouseY / height - 0.5) * 4;
-  const dirX = (mouseX / width - 0.5) * 4;
-  directionalLight(204, 204, 204, dirX, dirY, 1);
-  translate(-1.5 * radius, 0, 0);
-  //sphere(radius);
-  translate(3 * radius, 0, 0);
-  texture(img);
-  model(moon);
+  // const dirY = (mouseY / height - 0.5) * 8;
+  // const dirX = (mouseX / width - 0.5) * 8;
+  // directionalLight(204, 204, 204, dirX*2, dirY*2, .1);
+  translate(0 * radius, 0, 0);
+  rotateZ(theta * 1);
+  rotateX(theta * 1.01);
+  rotateY(theta * 1.01);
+  texture(vid);
+  model(moon, mouseX, mouseY);
+    theta += 0.05;
+
 }
