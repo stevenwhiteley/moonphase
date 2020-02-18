5f5387fd35a1a2849f76fd9a71ca326f03571282
Steven <stevenwhiteley4@gmail.com>
2020-02-18 13:50:23 -0800

    moon2

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
+const radius = 200;
+let moon;
+let moontexture;
+
 function setup() {
-  // put setup code here
+  createCanvas(710, 400, WEBGL);
+  noStroke();
+  fill(200);
+  moon = loadModel('assets/moon.obj', true);
+  moontexture = loadImage('assets/moontexture.jpg', true);
 }
 
 function draw() {
-  // put drawing code here
+  noStroke();
+  background(0);
+  const dirY = (mouseY / height - 0.5) * 4;
+  const dirX = (mouseX / width - 0.5) * 4;
+  directionalLight(204, 204, 204, dirX, dirY, 1);
+  translate(-1.5 * radius, 0, 0);
+  //sphere(radius);
+  translate(3 * radius, 0, 0);
+  texture(moontexture);
+  model(moon);
 }
